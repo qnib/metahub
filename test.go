6ea204fcfa5d967f4f3e40e028daf620fe7b46af
Robert Fujara <robert.fujara@gmail.com>
2019-04-07 14:40:25 +0200

    -

--- test.go
+++ test.go
@@ -12,4 +12,13 @@ func init() {
 func handleTest(w http.ResponseWriter, r *http.Request) {
 	//ctx := r.Context()
 	fmt.Fprint(w, "test")
+
+    //Accept header
+    // application/vnd.docker.distribution.manifest.v2+json
+    // application/vnd.docker.distribution.manifest.list.v2+json
+    // return Content-Type
+
+    // https://docs.docker.com/registry/spec/api/#manifest
+
+    // /v2/test/manifests/latest
 }
